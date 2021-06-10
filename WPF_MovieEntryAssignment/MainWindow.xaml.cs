a839c83bd5ca0cfb15a05969b1277bc77074b9db
mhamner <37221769+mhamner@users.noreply.github.com>
2021-06-10 16:58:07 -0400

    Add files via upload

---
+++ WPF_MovieEntryAssignment/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_MovieEntryAssignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Entry for Movie {movieTitle.Text} from {yearReleased.Text} accepted.");
+
+            //Clear the fields for the next entry
+            movieTitle.Text = "";
+            genre.Text = "";
+            director.Text = "";
+            yearReleased.Text = "";
+            averageStarRating.Text = "";
+        }
+    }
+}
